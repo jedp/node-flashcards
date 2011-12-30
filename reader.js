aabd1f1fffacb8ff3291cccb9c05c7570e80f4a3
Jed Parsons <jedp@me.com>
2011-12-29 16:29:31 -0800

    now can read json files for verb inflections

--- reader.js
+++ reader.js
@@ -1,8 +1,30 @@
 var fs = require('fs');
 
+/*
+ * Read a vocab file.
+ * Can be .txt or .json.
+ */
+
 module.exports.read = function read(filename, callback) {
   callback = callback || function() {};
 
+  if (filename.match(/\.txt$/)) {
+    return txt_reader(filename, callback);
+  } else if (filename.match(/\.json$/)) {
+    return json_reader(filename, callback);
+  } else {
+    return callback (new Error("Don't know how to read " + filename));
+  }
+}
+
+/*
+ * Get vocab from a text file.
+ *
+ * Structure must be pairs of lines separated by at least two newlines.
+ * First line is word to translate, second line is translation.
+ */
+
+function txt_reader(filename, callback) {
   fs.readFile(filename, function(err, buf) {
     if (err) return callback(err);
 
@@ -26,3 +48,41 @@ module.exports.read = function read(filename, callback) {
     return callback(null, vocab);
   });
 }
+
+/*
+ * get vocab from a json file
+ *
+ * structure must be:
+ * {word1:
+ *   {form1: "a string", 
+ *    form2: ['or', 'some', 'inflections'],
+ *    ...},
+ *  word2: { ... }, 
+ *  ...
+ *  }
+ */
+function json_reader(filename, callback) {
+  fs.readFile(filename, function(err, buf) {
+    if (err) return callback(err);
+
+    var vocab = [];
+    var data = JSON.parse(buf.toString());
+    for (var word in data) {
+      if (data.hasOwnProperty(word)) {
+        for (var form in data[word]) {
+          if (data[word].hasOwnProperty(form)) {
+            var to = data[word][form];
+            if (typeof(to) === 'string') {
+              vocab.push( [word + ': ' + form, to] );
+            } else {
+              vocab.push( [word + ': ' + form, to.join(', ')] );
+            }
+          }
+        }
+      }
+    }
+
+    return callback(null, vocab);
+  });
+}
+
