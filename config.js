b1298b2c667957a63dd42b924a91657541b998b7
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-03-15 09:26:21 -0600

    allow specification of REDIS_PORT in the environment

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  redis_port: 6385,
+  redis_port: process.env['REDIS_PORT'] || 6385,
   redis_host: '127.0.0.1'
+};
+
+if (typeof module.exports.redis_port !== 'number') {
+  module.exports.redis_port = parseInt(module.exports.redis_port, 10);
 }
